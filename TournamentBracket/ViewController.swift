37dde72d5a0e6a532c38047a978d01639f9d8216
Omar <omar@misfer.local>
2018-07-03 19:53:41 -0500

    Initial Commit

---
+++ TournamentBracket/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TournamentBracket
+//
+//  Created by Omar on 19/10/1439 AH.
+//  Copyright © 1439 Omar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
