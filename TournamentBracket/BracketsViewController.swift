e437163b258045113308a5508f8485132c7b08fe
Omar <omar@misfer.local>
2018-07-07 17:12:28 -0500

    initial commit

---
+++ TournamentBracket/BracketsViewController.swift
@@ -0,0 +1,118 @@
+//
+//  BracketsViewController.swift
+//  TournamentBracket
+//
+//  Created by Omar on 22/10/1439 AH.
+//  Copyright © 1439 Omar. All rights reserved.
+//
+
+import UIKit
+
+class BracketsViewController: UIViewController {
+    //--------Outlets------------
+    @IBOutlet weak var player1L: UILabel!
+    @IBOutlet weak var player2L: UILabel!
+    @IBOutlet weak var player3L: UILabel!
+    @IBOutlet weak var player4L: UILabel!
+    @IBOutlet weak var Splayer1L: UILabel!
+    @IBOutlet weak var Splayer2L: UILabel!
+    @IBOutlet weak var winnerL: UILabel!
+    
+    //--------Variables----------
+    var players = [String]()
+    var firstMatches = [UILabel]()
+    var stillInTheGame = [UILabel]()
+    var winners = [String]()
+    var losers = [String]()
+    
+    //--------Funcs--------------
+    override func viewDidLoad() {
+        
+        firstMatches = [player1L, player2L, player3L, player4L]
+        stillInTheGame = firstMatches
+        let labels = [player1L, player2L, player3L, player4L, Splayer1L, Splayer2L]
+        for label in labels{
+            label?.text = ""
+        }
+        
+        for i in 0..<players.count{
+            firstMatches[i].text = players[i]
+        }
+        startTournament()
+        super.viewDidLoad()
+    }
+
+    
+    func startTournament(){
+        
+    }
+    
+    @IBAction func onTappedLabel(_ sender: UITapGestureRecognizer) {
+        for label in firstMatches{
+            
+            if label.frame.contains(sender.location(in: view)){
+                if stillInTheGame.contains(label) && !winners.contains(label.text!) && !losers.contains(label.text!){
+                    // if the winner label is at an even index it means that the odd index has the loser and vice versa
+                    if players.index(of: label.text!)! % 2 == 0{
+                        let indexOfWinner = players.index(of: label.text!)!
+                        let indexOfLoser = indexOfWinner + 1
+                        results(winner: indexOfWinner, loser: indexOfLoser)
+                        
+                    }
+                    else if players.index(of: label.text!)! % 2 == 1{
+                        let indexOfWinner = players.index(of: label.text!)!
+                        let indexOfLoser = indexOfWinner - 1
+                        results(winner: indexOfWinner, loser: indexOfLoser)
+                    }
+                }
+                updateBracket()
+            }
+            
+        }
+    }
+    
+    func results(winner : Int , loser : Int){
+        //adds the winner to the winners' array and does the same for the loser
+        winners.append(players[winner])
+        losers.append(players[loser])
+    }
+    
+    func updateBracket(){
+        for i in 0..<winners.count{
+            for label in firstMatches{
+                if winners[i] == label.text{
+                    label.textColor = .green
+                }
+            }
+        }
+        
+        for i in 0..<losers.count{
+            for label in firstMatches{
+                if losers[i] == label.text{
+                    label.textColor = .red
+                }
+            }
+        }
+        
+        if winners.count+losers.count+1 >= firstMatches.count{
+            
+        }
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- TournamentBracket/ViewController.swift
+++ TournamentBracket/ViewController.swift
@@ -9,12 +9,84 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    //------------Variables------------
+    var players = [String]()
+    var playerLabels = [UILabel]()
+    var canAdvance = false
+    
+    //------------Outlet Parking-------
+    @IBOutlet weak var PlayerEntryTextField: UITextField!
+    @IBOutlet weak var player0: UILabel!
+    @IBOutlet weak var player1: UILabel!
+    @IBOutlet weak var player2: UILabel!
+    @IBOutlet weak var player3: UILabel!
+    @IBOutlet weak var player4: UILabel!
+    @IBOutlet weak var player5: UILabel!
+    @IBOutlet weak var player6: UILabel!
+    @IBOutlet weak var player7: UILabel!
+    @IBOutlet weak var player8: UILabel!
+    @IBOutlet weak var player9: UILabel!
+    @IBOutlet weak var advanceBut: UIButton!
+    
+    
+    
+    //------------Funcs----------------
+    @IBAction func addButton(_ sender: Any) {
+        
+        // Enters player in the players' array
+        if PlayerEntryTextField.text != "" && !(players.contains(PlayerEntryTextField.text!)){
+            players.append(PlayerEntryTextField.text!)
+            PlayerEntryTextField.text = ""
+            updateLabels()
+        }
+        
+        if players.contains(PlayerEntryTextField.text!){
+            let copyAlert = UIAlertController(title: "You can't input the same name twice!", message: nil, preferredStyle: .alert)
+            let copyAlertAction = UIAlertAction(title: "OK", style: .default)
+            copyAlert.addAction(copyAlertAction)
+            present(copyAlert, animated: true, completion: nil)
+        }
+        
+        if players.count > 1 {
+            advanceBut.setTitleColor( .blue , for: .normal)
+            canAdvance = true
+        }
+    }
+    
+    func updateLabels(){
+        for i in 0..<players.count{
+            if i < playerLabels.count{
+            playerLabels[i].text = players[i]
+            }
+            else{ playerLabels[playerLabels.count - 1].text = "..." }
+        }
+    }
+    
+    @IBAction func advanceButton(_ sender: Any) {
+        if canAdvance{
+            performSegue(withIdentifier: "segue1", sender: self)
+        }
+    }
+    
     override func viewDidLoad() {
+        playerLabels = [player0,player1,player2,player3,player4,player5,player6,player7,player8,player9]
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    override func viewWillAppear(_ animated: Bool) {
+        for label in playerLabels{
+            label.text = ""
+        }
+        players = [String]()
+        PlayerEntryTextField.text = ""
+    }
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let dataTunnel = segue.destination as! BracketsViewController
+        dataTunnel.players = players
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
